de7126d371228d39dc0e5b75166c802280a79a4b
Arne Wiese <arne.wiese@gmail.com>
2017-10-23 13:20:46 +0200

    Fixed tests

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,6 @@
-import * as express from 'express';
-import * as logger from "morgan";
-import * as compression from "compression";
+import app from "./app";
 
-import * as apiController from "./controllers/api";
-
-const server = express();
-server.use(compression());
-server.use(logger("dev"));
-server.set("port", process.env.PORT || 2015);
-server.set('x-powered-by', false);
-
-server.get("/api", apiController.getApi);
-
-server.listen(server.get("port"), () => {
-  console.log(("  App is running at http://localhost:%d in %s mode"), server.get("port"), server.get("env"));
+app.listen(app.get("port"), () => {
+  console.log(("  App is running at http://localhost:%d in %s mode"), app.get("port"), app.get("env"));
   console.log("  Press CTRL-C to stop\n");
 });
--- test/api.test.ts
+++ test/api.test.ts
@@ -1,16 +1,19 @@
 import {} from 'jest';
-import * as supertest from 'supertest';
 
-const request = supertest('http://localhost:8000');
+const request = require('supertest');
+
+import app from '../src/app';
 
 describe('GET /api', () => {
-  it('should return 200 OK', () => {
-    request.get('/api').expect(200);
-  });
+  it('should return 200 OK', () =>
+    request(app)
+      .get('/api')
+      .expect(200));
 });
 
 describe('GET /api/notHere', () => {
-  it('should return 404 not found', () => {
-    request.get('/api').expect(404);
-  });
+  it('should return 404 not found', () =>
+    request(app)
+      .get('/api/notHere')
+      .expect(404));
 });
