dc654955c11de6f24899216c818dc90ba731db0e
Arne Wiese <arne.wiese@gmail.com>
2017-10-24 16:04:07 +0200

    Updated tests and prettified everything

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,15 @@
 import * as express from 'express';
-import * as logger from "morgan";
-import * as compression from "compression";
+import * as logger from 'morgan';
+import * as compression from 'compression';
 
-import * as apiController from "./controllers/api";
+import * as apiController from './controllers/api';
 
 const app = express();
 app.use(compression());
-app.use(logger("dev"));
-app.set("port", process.env.PORT || 2015);
+app.use(logger('dev'));
+app.set('port', process.env.PORT || 2015);
 app.set('x-powered-by', false);
 
-app.get("/api", apiController.getApi);
+app.get('/api', apiController.getApi);
 
 export default app;
--- test/api.test.ts
+++ test/api.test.ts
@@ -4,16 +4,18 @@ const request = require('supertest');
 
 import app from '../src/app';
 
-describe('GET /api', () => {
-  it('should return 200 OK', () =>
-    request(app)
-      .get('/api')
-      .expect(200));
-});
+describe('Test the /api routes', () => {
+  describe('GET /api', () => {
+    it('should return 200 OK', () =>
+      request(app)
+        .get('/api')
+        .expect(200));
+  });
 
-describe('GET /api/notHere', () => {
-  it('should return 404 not found', () =>
-    request(app)
-      .get('/api/notHere')
-      .expect(404));
+  describe('GET /api/notHere', () => {
+    it('should return 404 not found', () =>
+      request(app)
+        .get('/api/notHere')
+        .expect(404));
+  });
 });
